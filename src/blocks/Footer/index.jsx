6912e09de2a40b8de39d3f38ab13a85b282b4adc
Victor <gamesvictor1928@gmail.com>
2023-10-14 18:30:14 -0300

    Fonte de texto

--- src/blocks/Footer/index.jsx
+++ src/blocks/Footer/index.jsx
@@ -5,8 +5,8 @@ export default function Footer(p) {
   return (
     <footer id={p.id} >
       <div className={content}>
-        <div title={p.title}>{p.title}</div>
-        <div>&copy; {(new Date).getFullYear()}, {p.title}</div>
+        <div title="Vanessa">Vanessa</div>
+        <div>&copy; {(new Date).getFullYear()}, Vanessa</div>
         {p.action && <Button href={p.actionHref} onClick={p.onActionClick} type="secondary" location="accent">{p.action}</Button>}
       </div>
     </footer>
