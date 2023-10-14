e9e87af7b9308298e72ef15f3c3dc31b191330ae
Victor <gamesvictor1928@gmail.com>
2023-10-14 20:41:15 -0300

    Mexendo no header e no parallax

--- src/components/SubscribeInput/index.jsx
+++ src/components/SubscribeInput/index.jsx
@@ -7,7 +7,7 @@ import styles, { container, button, input } from './style.module.scss';
 export default function SubscribeInput(p) {
   const [wppText, setWppText] = useState("https://wa.me/5511956600892?text=Ol%C3%A1%2C%20gostaria%20de%20um%20or%C3%A7amento%20para%20o%20meu%20projeto.")
   const location = p.location ?? 'body';
-
+  
   return (
     <div className={[container, styles[`container-location-${location}`]].join(" ")}>
       <input 
