b123b5b9ae6a838249577ec4d87670c816e9bcd2
Victor <gamesvictor1928@gmail.com>
2023-10-14 10:52:49 -0300

    tentando levar para o vercel

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const { join } = require('path')
 
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ["src"],
+  },
   webpack: (config) => {
     config.resolve.alias['@blocks'] = join(__dirname, 'src/blocks');
     config.resolve.alias['@components'] = join(__dirname, 'src/components');
