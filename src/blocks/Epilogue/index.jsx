873b7c039762bb9efdad39766dda5c8e69a0cd20
Victor <gamesvictor1928@gmail.com>
2023-10-15 11:41:43 -0300

    texto

--- src/blocks/Epilogue/index.jsx
+++ src/blocks/Epilogue/index.jsx
@@ -1,11 +1,15 @@
 import Section from '@components/Section';
 import SubscribeInput from '@components/SubscribeInput';
 import { container, header, illustration, subscribe, subtitle, title } from './style.module.scss';
+import professionalDesign from '../../../public/Images/professionalDesign.png';
+import Image from 'next/image';
 
 export default function Epilogue() {
   return (
     <Section id="epilogue" background="accent" className={container}>
-      <div className={illustration}></div>
+      <div className={illustration}>
+        {/* <Image src={professionalDesign} alt="Professional Design" layout='responsive' width={50} height={50}/> */}
+      </div>
       <div className={header}>
         <h2 className={title}>Otimizado para Sucesso</h2>
         <div className={subtitle}>Comece hoje a investir em seu sucesso profissional. Vamos criar um currículo que o destaque, abrindo portas e criando oportunidades. Sua carreira merece isso. Você merece isso.</div>
