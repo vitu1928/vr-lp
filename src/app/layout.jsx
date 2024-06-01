3aa1ecf86cdfa2d938a6f350cdb143b8f51bb938
Victor <gamesvictor1928@gmail.com>
2024-06-01 10:20:51 -0300

    nothing

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -108,7 +108,7 @@ export default function Home() {
 
   return (
     <html lang="pt-br">
-      <head>
+      <head>                                                      
         <script
           id="tagmanager-main"
           strategy="afterInteractive"
